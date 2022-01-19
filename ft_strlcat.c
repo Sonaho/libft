f05224bf1eca7a520572a316b4c933d3bd7d3ad6
Sonaho <74593956+Sonaho@users.noreply.github.com>
2022-01-19 11:22:10 +0100

    Actualizión

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalmela- <aalmela-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/13 12:40:04 by aalmela-          #+#    #+#             */
+/*   Updated: 2022/01/17 09:44:04 by aalmela-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-size_t ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t i;
-	size_t j;
-	size_t d_size;
-	size_t s_size;
+	size_t	i;
+	size_t	j;
+	size_t	d_size;
+	size_t	s_size;
 
 	d_size = ft_strlen(dst);
 	s_size = ft_strlen(src);
@@ -13,7 +25,6 @@ size_t ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize
 	j = d_size;
 	if (dstsize == 0)
 		return (s_size);
-
 	if (d_size < (dstsize - 1))
 	{
 		while (src[i] && ((d_size + i) < (dstsize - 1)))
