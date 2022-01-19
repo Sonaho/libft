b009f5da1c66d5669a5e2f00f036393f13358b40
Sonaho <74593956+Sonaho@users.noreply.github.com>
2022-01-19 16:33:45 +0100

    Update ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,24 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aalmela- <aalmela-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/13 14:41:29 by aalmela-          #+#    #+#             */
-/*   Updated: 2022/01/17 09:47:46 by aalmela-         ###   ########.fr       */
+/*   Created: 2022/01/13 11:27:56 by aalmela-          #+#    #+#             */
+/*   Updated: 2022/01/13 16:25:59 by aalmela-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t count, size_t size)
+void	ft_bzero(void *s, size_t n)
 {
-	void	*r;
-
-	r = (void *)malloc(count * size);
-	if (r == NULL)
-		return (NULL);
-	ft_bzero (r, count);
-	return (r);
+	ft_memset(s, '\0', n);
 }
