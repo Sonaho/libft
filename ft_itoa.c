caed2db9096092fc41e4e91c5cceaa4d60d37b89
Sonaho <74593956+Sonaho@users.noreply.github.com>
2022-01-20 13:35:13 +0100

    Update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,29 +6,62 @@
 /*   By: aalmela- <aalmela-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 15:23:03 by aalmela-          #+#    #+#             */
-/*   Updated: 2022/01/19 16:31:54 by aalmela-         ###   ########.fr       */
+/*   Updated: 2022/01/19 17:10:43 by aalmela-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
+static int	ft_countnum(int num)
+{
+	int	i;
+
+	i = 0;
+	if (num < 0);
+		num = num * -1;
+	printf("ft_countnum %d \n", i);
+	while (num > 0)
+	{
+		num /= 10;
+		i++;
+		printf("ft_countnum %d \n", i);
+	}
+	return (i);
+}
+
+static void	*ft_casezero(void)
+{
+	char	*ret;
+
+	ret = (char *)malloc(2);
+	ret[0] = 48;
+	ret[1] = 0;
+	return (ret);
+}
+
 char	*ft_itoa(int n)
 {
 	char	*ret;
 	char	*tmp;
-	int		mod;
+	int		cnt;
 	int		i;
 
-	i = 1;
-	mod = 0;
-	tmp = (char)malloc(i);
-	while (n > 0)
+	i = 0;
+	if (n == 0)
+		ret = ft_casezero();
+	else
 	{
-		mod = n % 10;
-		n = n / 10;
-		tmp[0] = mod + 48;
-		break ;
+		cnt = ft_countnum(n);
+		ret = (char *)malloc(cnt);
+		ret[0] = 0;
+		while (n > 0)
+		{
+			ret[(cnt - 1) - i] = (n % 10) + 48;
+			n /= 10;
+			i ++;
+		}
+		ret[i] = 0;
 	}
-	return (tmp);
+	return (ret);
 }
