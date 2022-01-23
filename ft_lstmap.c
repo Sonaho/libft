bb064b395dbfd9dc6c6ba10e339144bdd02d01fb
Sonaho <74593956+Sonaho@users.noreply.github.com>
2022-01-23 16:46:19 +0100

    Update ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -2,5 +2,20 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-    
+    t_list  *new_lst;
+    int     size;
+
+    if (!lst)
+        return ;
+    size = ft_lstsize(lst);
+    new_lst = (t_list *)malloc(sizeof(t_list) * size);
+    while (lst)
+    {
+        new_lst->content = f(lst->content);
+        if (!new_lst)
+            ft_lstdelone(*lst, del);
+        else
+            new_lst ++;
+    }
+    return (new_lst);
 }
