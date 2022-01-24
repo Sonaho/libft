5b02eef2104bcc9dfead08f133334627467fe580
Sonaho <74593956+Sonaho@users.noreply.github.com>
2022-01-24 17:05:36 +0100

    Update ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,30 +6,37 @@
 /*   By: aalmela- <aalmela-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 12:31:17 by aalmela-          #+#    #+#             */
-/*   Updated: 2022/01/24 14:16:48 by aalmela-         ###   ########.fr       */
+/*   Updated: 2022/01/24 17:02:33 by aalmela-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_lst;
-	t_list	*pos;
+	t_list	*tmp;
+	t_list	*new;
 
-	if (!lst || !f || !del)
+	if (!lst)
 		return (NULL);
-	new_lst = NULL;
-	pos = lst;
-	while (pos)
+	printf("%s \n", lst->content);
+	new = NULL;
+	while (lst)
 	{
-		if (!new_lst)
-			new_lst = ft_lstnew((*f)(pos->content));
+		tmp = ft_lstnew((*f)(lst->content));
+		printf("%s \n", tmp->content);
+		if (tmp)
+			ft_lstadd_back(&new, tmp);
 		else
-			ft_lstadd_back(&new_lst, ft_lstnew((*f)(pos->content)));
-		if (!new_lst->content)
-			(*del)(new_lst->content);
-		pos = pos->next;
+		{
+			while (new)
+			{
+				ft_lstclear(&new, (*del));
+				return (0);
+			}
+		}
+		lst = lst->next;
 	}
-	return (new_lst);
+	return (new);
 }
