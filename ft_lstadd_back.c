239760447ada6d0123638c2c25af95f55304a4d7
Sonaho <74593956+Sonaho@users.noreply.github.com>
2022-01-24 10:53:40 +0100

    Update ft_lstadd_back.c

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -1,16 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalmela- <aalmela-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/24 10:25:09 by aalmela-          #+#    #+#             */
+/*   Updated: 2022/01/24 10:52:36 by aalmela-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-    t_list t_last;
+	t_list	*t_last;
 
-    if (!lst || !new)
-        return;
-    if (*lst == NULL)
-        return;
-    else
-    {
-        t_last = ft_lstlast(*lst);
-        t_last->next = new;
-    }
+	if (lst)
+	{
+		if (*lst)
+		{
+			t_last = ft_lstlast(*lst);
+			t_last->next = new;
+		}
+		else
+			*lst = new;
+	}	
 }
