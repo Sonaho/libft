152a66d69d27567f6e64b70a83680cef69079563
Sonaho <74593956+Sonaho@users.noreply.github.com>
2022-01-31 16:13:41 +0100

    Add files via upload

--- ft_split.c
+++ ft_split.c
@@ -6,14 +6,13 @@
 /*   By: aalmela- <aalmela-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 15:06:48 by aalmela-          #+#    #+#             */
-/*   Updated: 2022/01/19 15:15:48 by aalmela-         ###   ########.fr       */
+/*   Updated: 2022/01/31 16:04:10 by aalmela-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-static size_t ft_wordlen(const char *s, char c)
+static size_t	ft_wordlen(const char *s, char c)
 {
 	size_t	size;
 	size_t	i;
@@ -54,33 +53,27 @@ char	**ft_split(char const *s, char c)
 {
 	int		i;
 	int		j;
-	int		y;
-	int		cnt;
 	char	**split;
 
-	cnt = ft_countchars(s, c);
-	printf("\nTamaño countchar %d \n",cnt);
-	split = (char **)ft_calloc(cnt + 1, sizeof(char *));
+	split = (char **)ft_calloc(ft_countchars(s, c) + 1, sizeof(char *));
 	if (split == NULL)
 		return (NULL);
 	i = 0;
 	j = 0;
-	y = 0;
-	while (cnt > 0)
+	while (s[i])
 	{
 		while (s[i] == c)
-			i ++;
+			i++;
 		if (s[i])
 		{
-			split[j] = (char *)ft_calloc(ft_wordlen(s + i, c) + 1, sizeof(char));
+			split[j] = (char *)ft_calloc(ft_wordlen(s + i, c) + 1,
+					sizeof(char));
 			if (!split[j])
 				return (NULL);
-			while (s[i] && s[i] != c)
-				split[j][y++] = s[i++];
+			ft_strlcpy(split[j], s + i, ft_wordlen(s + i, c) + 1);
+			i += ft_wordlen(s + i, c);
 			j ++;
 		}
-		cnt --;
 	}
-	split[j] = 0;
 	return (split);
 }
